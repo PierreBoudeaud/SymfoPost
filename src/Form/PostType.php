5f81b4c9d79cb650f98a627d2714376e2dc1306d
Pierre Boudeaud <pboudeaud@gmail.com>
2019-02-10 00:11:52 +0100

    Create post form

---
+++ src/Form/PostType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'label.title'
+            ])
+            ->add('Content', TextareaType::class)
+            ->add('status')
+            ->add('tags', EntityType::class, [
+                'multiple' => true,
+                'class' => Tag::class,
+                'choice_label' => 'label'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'btn.create',
+                'attr' => [
+                    'class' => 'btn btn-outline-primary'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
